d36a8c4f2b94c810b98b85632f79f032ec65b36e
Lena Kharchenko <elenkakharchenko@gmail.com>
2023-04-17 18:55:36 +0300

    update js

---
+++ src/index.js
@@ -0,0 +1,89 @@
+import './css/styles.css';
+import { fetchCountries } from './api/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  countryInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.countryInput.addEventListener(
+  'input',
+  debounce(searchCountryFunc, DEBOUNCE_DELAY)
+);
+
+function searchCountryFunc(evt) {
+  evt.preventDefault();
+  const countryName = evt.target.value.trim();
+  if (!countryName) {
+    resetMarkup(refs.countryList);
+    resetMarkup(refs.countryInfo);
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(dataCountry => {
+      if (dataCountry.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (dataCountry.length >= 2 && dataCountry.length <= 10) {
+        resetMarkup(refs.countryList);
+        createMarkupCountryList(dataCountry);
+        resetMarkup(refs.countryInfo);
+      } else {
+        resetMarkup(refs.countryInfo);
+        createMarkupCountryInfo(dataCountry);
+        resetMarkup(refs.countryList);
+      }
+    })
+    .catch(() => {
+      resetMarkup(refs.countryList);
+      resetMarkup(refs.countryInfo);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function createMarkupCountryList(dataCountry) {
+  const markup = dataCountry
+    .map(({ name, flags }) => {
+      return `<li class="country-list__item">
+          <img class="country-list__img" src="${flags.svg}" alt="flag" />
+          <p class="country-list__text">${name.official}</p>
+        </li>`;
+    })
+    .join('');
+  return refs.countryList.insertAdjacentHTML('beforeend', markup);
+}
+
+function createMarkupCountryInfo(dataCountry) {
+  const markup = dataCountry
+    .map(({ name, capital, population, flags, languages }) => {
+      return `
+    <div class="country__flag">
+      <img class="country__img" src="${flags.svg}" alt="flag">
+      <p class="country__name">${name.official}</p>
+    </div>
+    <ul class="country__info">
+        <li class="country__item"> <b>Capital</b>:
+      <span class="country__span">${capital}</span>
+        </li>
+        <li class="country__item"> <b>Population</b>:
+      <span class="country__span">${population}</span>
+        </li>
+        <li class="country__item"> <b>Languages</b>:
+      <span class="country__span">${Object.values(languages).join(', ')}</span>
+        </li>
+    </ul>`;
+    })
+    .join('');
+
+  return refs.countryInfo.insertAdjacentHTML('beforeend', markup);
+}
+
+function resetMarkup(el) {
+  el.innerHTML = '';
+}
